b55ac6d1ea10f206f3c173942a7d549fd9edcd2e
buzzon <buzzondev@gmail.com>
2020-06-11 04:44:57 +0500

    first step

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+	text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.text
